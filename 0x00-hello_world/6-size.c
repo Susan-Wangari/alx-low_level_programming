2c3c5813460aadced27e30817764508ab86a32d8
Susan-Wangari <susanwangari810@gmail.com>
2022-06-30 14:53:32 -0700

    print size of characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,8 @@ int main()
 	    printf("%lu\n", sizeof(char));
 	        printf("%lu\n", sizeof(int));
 		    printf("%lu\n", sizeof(float));
-		        printf("%lu", sizeof(double));
-			    return 0;
+		        printf("%lu\n", sizeof(long int));
+		           printf("%lu\n", sizeof(long long int));
+		              printf("%lu", sizeof(double));
+			      return 0;
 }
