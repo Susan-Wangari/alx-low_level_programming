09b6a51d6dd7bb3c8b495da096f232023985a75e
Susan-Wangari <susanwangari810@gmail.com>
2022-06-30 14:38:01 -0700

    print a string using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *   * main -entry point to the program
- *     *Description: print text using the put function
- *       * Return: Always 0 (Success)
- *         */
+ *    main -entry point to the program
+ *    Description: print text using the put function
+ *    Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
