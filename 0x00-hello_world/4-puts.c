902ef51f01a39e7bf7c17accb3c16efc9313275b
Susan-Wangari <susanwangari810@gmail.com>
2022-06-30 14:26:30 -0700

    print a string using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #!/bin/bash 
-puts("Programming is like building a multilingual puzzle")  
+#include <stdio.h>
+/**
+ *Main-the entry point to the program
+ *Description: print text using the put function
+ *Return: Always 0(success)
+ */
+ int main (void) {
+   puts("Programming is like building a multilingual puzzle");
+   return(0);
+ }  
