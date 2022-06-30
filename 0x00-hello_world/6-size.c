6ee05fd3438c238e862a895c458bcf321ccab26d
Susan-Wangari <susanwangari810@gmail.com>
2022-06-30 15:16:36 -0700

    display size of characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	    printf("%lu\n", sizeof(char));
-	        printf("%lu\n", sizeof(int));
-		    printf("%lu\n", sizeof(long int));
-		           printf("%lu\n", sizeof(long long int));
-		              printf("%lu\n", sizeof(float));
+	    printf("%d\n", sizeof(char));
+	        printf("%d\n", sizeof(int));
+		    printf("%d\n", sizeof(long int));
+		           printf("%d\n", sizeof(long long int));
+		              printf("%d\n", sizeof(float));
 			      return 0;
 }
